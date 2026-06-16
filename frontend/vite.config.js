e89a2dad6f8b8f3b18a0e65a45a16855c7e723d6
abiha-aftab <abiha-aftab@users.noreply.github.com>
2026-06-16 15:55:51 +0500

    chore: clean up build/editor config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
